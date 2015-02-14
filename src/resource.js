9227e14b6da702100fdfedc88832f9a3ec9e89e0
Robin Smith <robin@mrrobinsmith.com>
2015-02-14 11:09:50 +0000

    New branding

--- src/resource.js
+++ src/resource.js
@@ -1,8 +1,8 @@
 cc.log("Setting resource variables ...")
 var res = {
-    helloBG_png : "res/helloBG.png",
-    start_n_png : "res/start_n.png",
-    start_s_png : "res/start_s.png",
+    helloBG_png : "res/bouncing-ball-bg.jpg",
+    start_n_png : "res/start_n-green.png",
+    start_s_png : "res/start_s-green.png",
     PlayBG_png  : "res/PlayBG.png",
     runner_png  : "res/running.png",
     runner_plist : "res/running.plist",
