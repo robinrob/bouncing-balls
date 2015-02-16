875f9af974d802d2eeb9e7415a029d0ede7d2fec
Robin Smith <robin@mrrobinsmith.com>
2015-02-16 19:23:32 +0000

    Wrap globals in object

--- src/globals.js
+++ src/globals.js
@@ -1,22 +1,24 @@
 cc.log("Setting globals ...")
-var g_groundHeight = 57;
-var g_runnerStartX = 80;
+var mrrobinsmith = {
+    g_groundHeight = 57;
+    g_runnerStartX = 80;
 
-var g_buttonOuterColor = new cc.color(0, 255, 0, 0)
-var g_buttonInnerColor = new cc.color(0, 0, 255, 0)
-var g_buttonTextColor = new cc.color(0, 255, 0, 0)
+    g_buttonOuterColor = new cc.color(0, 255, 0, 0)
+    g_buttonInnerColor = new cc.color(0, 0, 255, 0)
+    g_buttonTextColor = new cc.color(0, 255, 0, 0)
 
-if(typeof TagOfLayer == "undefined") {
-    var TagOfLayer = {};
-    TagOfLayer.background = 0;
-    TagOfLayer.Animation = 1;
-    TagOfLayer.Status = 2;
-};
+    if(typeof TagOfLayer == "undefined") {
+        var TagOfLayer = {};
+        TagOfLayer.background = 0;
+        TagOfLayer.Animation = 1;
+        TagOfLayer.Status = 2;
+    };
 
 // collision type for chipmunk
-if(typeof SpriteTag == "undefined") {
-    var SpriteTag = {};
-    SpriteTag.runner = 0;
-    SpriteTag.coin = 1;
-    SpriteTag.rock = 2;
-};
+    if(typeof SpriteTag == "undefined") {
+        var SpriteTag = {};
+        SpriteTag.runner = 0;
+        SpriteTag.coin = 1;
+        SpriteTag.rock = 2;
+    };
+}
