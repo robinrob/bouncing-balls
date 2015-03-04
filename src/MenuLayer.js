51f47bea48fc04dacdb021240a8326a20033ef8e
Robin Smith <robin@mrrobinsmith.com>
2015-03-04 13:00:36 +0000

    Strip down MenuLayer a bit

--- src/MenuLayer.js
+++ src/MenuLayer.js
@@ -1,31 +1,20 @@
 var MenuLayer = cc.Layer.extend({
     ctor : function(){
         cc.log("MenuLayer.ctor ...")
-        //1. call super class's ctor function
         this._super();
     },
     init:function(){
         cc.log("MenuLayer.init ...")
-        //call super class's super function
         this._super();
 
-        //2. get the screen size of your game canvas
         var winsize = cc.director.getWinSize();
-
-        //3. calculate the center point
         var centerpos = cc.p(winsize.width / 2, winsize.height / 2);
 
-        //4. create a background image and set it's position at the center of the screen
-        var spritebg = new cc.Sprite(mrrobinsmith.res.menu_background_png);
-        spritebg.setPosition(centerpos);
-        this.addChild(spritebg);
-
-        //5.create a menu and assign onPlay event callback to it
         var menuItemPlay = new cc.MenuItemSprite(
             ui.startButton().normal,
             ui.startButton().selected,
             this.onPlay, this);
-        var menu = new cc.Menu(menuItemPlay);  //7. create the menu
+        var menu = new cc.Menu(menuItemPlay);
         menu.setPosition(centerpos);
         this.addChild(menu);
 
